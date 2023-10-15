44b52445672ec1b49c628eed8a1679a098ca5ff4
allvo6377 <alvinpilot@gmail.com>
2023-10-15 23:54:44 +0000

    done

--- markdown2html.py
+++ markdown2html.py
@@ -1,33 +1,16 @@
 import sys
+import os
 
-def markdown2html(markdown_file, output_file):
-  """Converts a Markdown file to HTML.
-
-  Args:
-    markdown_file: The path to the Markdown file.
-    output_file: The path to the output HTML file.
-  """
-
-  with open(markdown_file, "r") as f:
-    markdown_content = f.read()
-
-  html_content = markdown2html(markdown_content)
-
-  with open(output_file, "w") as f:
-    f.write(html_content)
-
-if __name__ == "__main__":
-  if len(sys.argv) < 3:
+if len(sys.argv) < 3:
     print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
     sys.exit(1)
 
-  markdown_file = sys.argv[1]
-  output_file = sys.argv[2]
+md_file = sys.argv[1]
+html_file = sys.argv[2]
 
-  if not os.path.exists(markdown_file):
-    print("Missing {}".format(markdown_file), file=sys.stderr)
+if not os.path.exists(md_file):
+    print(f"Missing {md_file}", file=sys.stderr)
     sys.exit(1)
 
-  markdown2html(markdown_file, output_file)
+sys.exit(0)
 
-  sys.exit(0)
